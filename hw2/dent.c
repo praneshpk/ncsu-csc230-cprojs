2bb4058bd74257ecb6231e2ea62719f20520831b
Pranesh Kamalakanthan <praneshpk@gmail.com>
2016-10-26 12:16:06 -0400

    hw2 commit

--- hw2/dent.c
+++ hw2/dent.c
@@ -9,14 +9,78 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /** 
-    Program starting point, reads text from standard input until end-of-file.
+    Program starting point, reads and prints out formatted text
+    from standard input until end-of-file
     
-    @return program exit status
+    @return program exit status, 100 if there are unmatched brackets
 */
-int main(){
+int main()
+{
+  // Initial indentation depth
+  int d = 0;
+  //bool indent = false;
+
   for(int ch = getchar(); ch != EOF; ch = getchar()){
+    // Discards hard tabs and spaces at beginning of line
+    if(ch == ' ' ||
+      ch == '\t' ) {
+      while( (ch = getchar()) == ' ' ||
+              ch == '\t' );
+    }
+
+    // Checks for closing bracket at the beginning and decrements depth
+    if( ch =='}' ){
+      d--;
+      // Prints error if there are more } brackets
+      if( d < 0 ){
+        printf("Unmatched brackets\n");
+        return 100;
+      }
+    }
+
+    // Adds soft tabs for non-empty line
+    if( ch !='\n' ) {
+      for(int i = 0; i < 2*d; i++)
+        putchar(' ');
+    }
+
+    // Checks for opening bracket and increments depth
+    if( ch == '{' )
+      d++;
+
+    // Print first character
+    putchar(ch);
+
+    // Add soft tabs for next character
+    if( ch =='\n' ) {
+      for(int i = 0; i < 2*d; i++)
+        putchar(' ');
+    }
+
+    // Read rest of line and increment or decrement depth based on bracket
+    for(ch = getchar(); ch != '\n'; ch = getchar()){
+      if( ch == '{' )
+        d++;
+      else if( ch == '}' ){
+        d--;
+        if( d < 0 ){
+          printf("Unmatched brackets\n");
+          return 100;
+        }
+      }
+      putchar(ch);
+    }
+    // Print last character
+    putchar(ch);
+  }
+
+  // Prints error if there are more { brackets
+  if( d > 0){
+    printf("Unmatched brackets\n");
+    return 100;
   }
   return 0;
 }
--- hw2/pie.c
+++ hw2/pie.c
@@ -5,3 +5,29 @@
   This program draws a pie chart given sizes for 3 pie slice (red, green, and blue).
   
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool insideCircle( int x, int y, int rad )
+{
+
+}
+bool insideSlice( int x, int y, ... )
+{
+
+}
+
+/** 
+    Program starting point, reads text from standard input until end-of-file.
+    
+    @return program exit status
+*/
+int main()
+{
+  // Pie slices input
+  int s1, s2, s3;
+  scanf("%d %d %d", s1, s2, s3);
+  
+}
